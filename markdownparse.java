86f0266dbfeac81dc2b09fa7b9fedccba26fab01
taixinw <twei@ucsd.edu>
2022-04-24 12:23:29 -0700

    fix test-file3

--- markdownparse.java
+++ markdownparse.java
@@ -20,6 +20,9 @@ public class MarkdownParse {
             if (openBracket == -1) {
                 break;
             }
+            if (closeBracket == -1){
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
