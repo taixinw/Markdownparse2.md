dfcb33a9dda5c91e3dbbf2598573a533ece866db
taixinw <twei@ucsd.edu>
2022-04-24 11:55:44 -0700

    fix test-file2 error

--- markdownparse.java
+++ markdownparse.java
@@ -16,6 +16,10 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            // fix test-file2 error
+            if (openBracket == -1) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
